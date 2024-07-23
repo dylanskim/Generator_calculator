9a8acc3a456a4101a40a1c94337e2706ed1c218d
IX <52508423+dylanskim@users.noreply.github.com>
2024-07-23 03:00:24 -0400

    Update Generator_calculator.py

--- Generator_calculator.py
+++ Generator_calculator.py
@@ -3,30 +3,12 @@ def num_abb (user_in: str):
     for i in user_in:
         if i.isalpha():
             expo = "".join([expo, i])
-    if expo.lower() == "":
-        multi = 1
-    if expo.lower() == "k":
-        multi = 10**3
-    if expo.lower() == "m":
-        multi = 10**6
-    if expo.lower() == "b":
-        multi = 10**9
-    if expo.lower() == "t":
-        multi = 10**12
-    if expo.lower() == "q":
-        multi = 10**15
-    if expo.lower() == "qi":
-        multi = 10**18
-    if expo.lower() == "s":
-        multi = 10**21
-    if expo.lower() == "sx":
-        multi = 10**24
-    if expo.lower() == "o":
-        multi = 10**27
-    if expo.lower() == "n":
-        multi = 10**30
-    if expo.lower() == "d":
-        multi = 10**33
+    
+    multi = 1
+    table = {"": 0, "k": 3, "m": 6, "b": 9, "t": 12, "q": 15, "qi": 18, "s": 21, "sx": 24, "0": 27, "n": 30, "d": 33}
+    for abbr, expon in table.items():
+        if expo.lower() == abbr:
+            multi = 10 ** expon
 
     num = ""
     for i in user_in:
@@ -36,8 +18,6 @@ def num_abb (user_in: str):
     float_out = float(num) * multi
     return float_out
 
-input
-    
 tick = num_abb(input("Generator base ticks: "))
 gen = num_abb(input("Generator power per tick: "))
 cost = num_abb(input("Generator cost: "))
