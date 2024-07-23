250cee663684d66ef105299be742613eec3435df
IX <52508423+dylanskim@users.noreply.github.com>
2024-07-23 15:51:15 -0400

    Update Generator_calculator.py

--- Generator_calculator.py
+++ Generator_calculator.py
@@ -5,7 +5,7 @@ def input_number_abbreviation(user_input: str):
             input_abbreviation = "".join([input_abbreviation, i])
     
     multiplier = 1
-    abbreviation_table = {"": 0, "k": 3, "m": 6, "b": 9, "t": 12, "q": 15, "qi": 18, "s": 21, "sx": 24, "0": 27, "n": 30, "d": 33}
+    abbreviation_table = {"": 0, "k": 3, "m": 6, "b": 9, "t": 12, "q": 15, "qi": 18, "s": 21, "sx": 24, "o": 27, "n": 30, "d": 33}
     for suffix, exponent in abbreviation_table.items():
         if input_abbreviation.lower() == suffix:
             multiplier = 10 ** exponent
@@ -15,25 +15,24 @@ def input_number_abbreviation(user_input: str):
         if not i.isalpha():
             number = "".join([number, i])
 
-    float_out = float(number) * multiplier
-    return float_out
+    return float(number) * multiplier
 
 def output_number_abbreviation(number_out: int):
     output_length = len(str(number_out))
     if output_length >= 4:
         divided_number = str(number_out / (10 ** (((output_length - 1) // 3) * 3)))
-    if output_length <= 4:  
+        print(((output_length - 1) // 3) * 3)
+    if output_length < 4:  
         divided_number = str(number_out)
-    abbreviation_table = {"": 0, "k": 3, "m": 6, "b": 9, "t": 12, "q": 15, "qi": 18, "s": 21, "sx": 24, "0": 27, "n": 30, "d": 33}
+    abbreviation_table = {"": 0, "k": 3, "m": 6, "b": 9, "t": 12, "q": 15, "qi": 18, "s": 21, "sx": 24, "o": 27, "n": 30, "d": 33}
     for suffix, exponent in abbreviation_table.items():
         if (((output_length - 1) // 3) * 3) == exponent:
-            if not len(divided_number) >= 6:
-                divided_number = divided_number + "000000"
-            if int(divided_number[5]) > 0:
-                divided_number = divided_number[:4] + str(int(divided_number[4]) + 1)
+            if len(divided_number) >= 6:
+                divided_number = divided_number[:5]
+                if int(divided_number[4]) > 0:
+                    divided_number = divided_number[:-1] + str(int(divided_number[-1]) + 1) #+1 for round up, -1 for round down
             
-            abbreviated_number = divided_number[:5] + suffix
-            return abbreviated_number   
+            return divided_number[:5] + suffix     
    
 tick = input_number_abbreviation(input("Generator base ticks: "))
 heat = input_number_abbreviation(input("Generator heat per tick: "))
