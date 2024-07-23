2d9c00bf0f9c4fb4a55d1e204f85859e3233ca0b
IX <52508423+dylanskim@users.noreply.github.com>
2024-07-23 02:39:07 -0400

    Update Generator_calculator.py

--- Generator_calculator.py
+++ Generator_calculator.py
@@ -6,27 +6,27 @@ def num_abb (user_in: str):
     if expo.lower() == "":
         multi = 1
     if expo.lower() == "k":
-        multi = 1*10**3
+        multi = 10**3
     if expo.lower() == "m":
-        multi = 1*10**6
+        multi = 10**6
     if expo.lower() == "b":
-        multi = 1*10**9
+        multi = 10**9
     if expo.lower() == "t":
-        multi = 1*10**12
+        multi = 10**12
     if expo.lower() == "q":
-        multi = 1*10**15
+        multi = 10**15
     if expo.lower() == "qi":
-        multi = 1*10**18
+        multi = 10**18
     if expo.lower() == "s":
-        multi = 1*10**21
+        multi = 10**21
     if expo.lower() == "sx":
-        multi = 1*10**24
+        multi = 10**24
     if expo.lower() == "o":
-        multi = 1*10**27
+        multi = 10**27
     if expo.lower() == "n":
-        multi = 1*10**30
+        multi = 10**30
     if expo.lower() == "d":
-        multi = 1*10**33
+        multi = 10**33
 
     num = ""
     for i in user_in:
@@ -38,7 +38,7 @@ def num_abb (user_in: str):
 
 input
     
-tick = num_abb(input("Generator ticks: "))
+tick = num_abb(input("Generator base ticks: "))
 gen = num_abb(input("Generator power per tick: "))
 cost = num_abb(input("Generator cost: "))
 
